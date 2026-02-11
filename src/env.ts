d3323383ae6d74bca5c3146b242cbba7519660a0
Anthony Perez <anthonyj.perez@gmail.com>
2026-02-10 23:28:22 -0500

    update runtimeEnv to handle node environments

--- src/env.ts
+++ src/env.ts
@@ -21,7 +21,7 @@ export const env = createEnv({
 	 * What object holds the environment variables at runtime. This is usually
 	 * `process.env` or `import.meta.env`.
 	 */
-	runtimeEnv: import.meta.env,
+	runtimeEnv: process?.env ?? import.meta.env,
 
 	/**
 	 * By default, this library will feed the environment variables directly to
